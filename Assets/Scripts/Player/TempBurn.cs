315e15c75265daa31a7a8ae633327cfcaa0cc673
Manny Patrick Vivas <mannyvivas21@gmail.com>
2026-02-27 11:32:10 -0500

    captain's quarters level

--- Assets/Scripts/Player/TempBurn.cs
+++ Assets/Scripts/Player/TempBurn.cs
@@ -7,6 +7,7 @@ public class TempBurn : MonoBehaviour
     RaycastHit hit;
     LineRenderer line;
     [SerializeField] LayerMask burn;
+    Lantern currentLantern;
     public bool refraction;
     [SerializeField] CinemachineCamera cam;
 
@@ -37,7 +38,25 @@ public class TempBurn : MonoBehaviour
 
         if (Physics.Raycast(transform.position, dir, out hit, 50.0f, burn))
         {
-            hit.transform.gameObject.GetComponent<Burnable>().ApplyBurn(Time.deltaTime);
+            if (hit.transform.gameObject.layer == 8)
+            {
+                hit.transform.gameObject.GetComponent<Burnable>().ApplyBurn(Time.deltaTime);
+            }
+            else
+            {
+                currentLantern = hit.transform.gameObject.GetComponent<Lantern>();
+                currentLantern.hitsThisFrame = 1;
+                if (currentLantern.activeLantern)
+                    GetComponentInParent<LanternTravel>().ActivatedLanterns.Add(currentLantern);
+            }
+        }
+        else
+        {
+            if (currentLantern != null && currentLantern.hitsThisFrame > 0)
+            {
+                currentLantern.hitsThisFrame = 0;
+                currentLantern = null;
+            }
         }
         //Debug.DrawRay(transform.position, dir * 50.0f, Color.magenta);
     }
